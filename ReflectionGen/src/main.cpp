9246177052809bad5ef10c250bf0976ac5dd740d
carl <carlmarxchen@foxmail.com>
2022-07-15 23:17:25 +0800

    more option

--- ReflectionGen/src/main.cpp
+++ ReflectionGen/src/main.cpp
@@ -1,5 +1,6 @@
 #include "CLI11.hpp"
 #include "ReflectionParser.h"
+#include "StringUtils.h"
 #include <algorithm>
 #include <atomic>
 #include <condition_variable>
@@ -126,7 +127,7 @@ static int DoScript(sol::state& lua, const std::string& scriptPath)
     } else {
         sol::error err = pr;
         std::cout << "Failed to do script " << scriptPath.c_str()
-                  << ": " << err.what();
+                  << ": " << err.what() << std::endl;
         return 1;
     }
 }
@@ -398,6 +399,7 @@ int main(int argc, char** argv)
     std::string compileDataBaseFile;
     std::vector<std::string> includeRegexs;
     std::vector<std::string> excludeRegexs;
+    std::vector<std::string> concatenatedClangParamsList;
     std::vector<std::string> dirs;
     std::vector<std::string> files;
     uint32_t workThreadsCount = std::max(std::thread::hardware_concurrency() / 2, 1U);
@@ -408,9 +410,30 @@ int main(int argc, char** argv)
     app.add_option("-f,--file", files, "A list of files which will be parsed");
     app.add_option("--include", includeRegexs, "A list of regex to filter in files, so that only they will not be processed");
     app.add_option("--exclude", excludeRegexs, "A list of regex to filter out files, so that they will not be processed");
+    app.add_option("--clang-params", concatenatedClangParamsList,
+        "A list of command params to be passed to libclang. The first letter will be used as delimiter,"
+        " which will then divide the rest into a list of arguments, which will be passed to libclang. e.g. ';-Idir1;-Idir2',"
+        " then we will pass '-Idir1' and '-Idir2' to libclang.");
     app.add_option<uint32_t>("-j,--jobs", workThreadsCount, "Concurrent parsing.");
 
     CLI11_PARSE(app, argc, argv);
+
+    for (auto& params : concatenatedClangParamsList) {
+        if (params.empty()) {
+            std::cerr << "You specified an empty string for --clang-params option, which is illegal, we will ignore it" << std::endl;
+            continue;
+        }
+        std::vector<std::string_view> list = StringUtils::Split(params, params[0]);
+        auto delimiter = params[0];
+        for (auto& c : params) {
+            if (c == delimiter) {
+                c = '\0';
+            }
+        }
+        for (size_t i = 1; i < list.size(); ++i) {
+            clangParams.push_back(list[i].data());
+        }
+    }
 
     std::vector<ParseTask> parseTasks;
     {
